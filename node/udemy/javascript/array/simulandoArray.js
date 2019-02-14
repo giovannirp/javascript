8084cb63268a10bd4671e66a2364e5b8f1ca164a
Giovanni Ribeiro Pinto <giovanni.ribeiro@grupoltm.com.br>
2019-02-14 19:43:44 -0200

    deletando diretorio não importante para o projeto

--- node/udemy/javascript/array/simulandoArray.js
+++
@@ -1,6 +0,0 @@
-const quaseArray = { 0: 'Rafael', 1: 'Ana', 2: 'Bia'}
-console.log(quaseArray)
-
-
-const meuArray = ['Rafale', 'Ana', 'Bia']
-console.log(quaseArray.toString(), meuArray)
--- node/udemy/javascript/heranca4.js
+++
@@ -1,8 +0,0 @@
-function MeuObjeto() {
-}
-
-
-console.log((new MeuObjeto).__phto__ === MeuObjeto.prototype);
-console.log(MeuObjeto.__phto__ === Function.prototype);
-console.log(Function.prototype.__phto__ === Object.prototype);
-console.log(Object.prototype.__phto__ === null)
